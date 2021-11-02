d67011047772a71861a2bc00ce3f924345fcf3f3
TAEKHWAN <b2narae@gmail.com>
2021-11-02 23:56:30 +0900

    Edit - SingletonBillPugh.java Comment

--- src/DesignPattern/SingletonPattern/SingletonBillPugh.java
+++ src/DesignPattern/SingletonPattern/SingletonBillPugh.java
@@ -9,6 +9,7 @@ getInstance()가 호출될 때 JVM 메모리에 로드되고, 인스턴스를 �
 synchronized를 사용하지 않기 때문에 성능 저하 또한 문제 없음.
 그러나, Java 의 Reflection으로 싱글톤을 파괴할 수 있으며, Enum으로 싱글톤을 구현하는 방법이 있긴 함.
 
+그러나 Enum으로 구현하는 방식은 많이 쓰이지 않음.
 java.lang.reflect.Constructor라는 놈의 constructor.setAccessible(true)를 하면 break 됨
  */
 public class SingletonBillPugh {
