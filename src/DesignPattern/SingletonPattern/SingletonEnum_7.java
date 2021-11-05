34ae29d1447e0387ed1620971989aaf12124a366
TAEKHWAN <b2narae@gmail.com>
2021-11-05 23:52:01 +0900

    Add - Singleton Enum Method

---
+++ src/DesignPattern/SingletonPattern/SingletonEnum_7.java
@@ -0,0 +1,12 @@
+package DesignPattern.SingletonPattern;
+
+/*
+Enum으로 구현시 직렬화/역직렬화에 대한 처리 필요 없음.
+ */
+public enum SingletonEnum_7 {
+    Instance;
+
+    public void someMethod(String param) {
+
+    }
+}
