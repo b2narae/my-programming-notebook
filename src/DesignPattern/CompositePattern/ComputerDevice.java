417bd46ef1b4c1aa540abe4e2cbb0b5df3fdde89
TAEKHWAN <b2narae@gmail.com>
2021-11-09 23:59:43 +0900

    Comment - Composite Pattern Client

--- src/DesignPattern/CompositePattern/ComputerDevice.java
+++ src/DesignPattern/CompositePattern/ComputerDevice.java
@@ -1,6 +1,7 @@
 package DesignPattern.CompositePattern;
 
 // 한데 묶어서 관리하고 싶은 기능들.
+// Computer에서 묶어서 관리하게 됨
 public abstract class ComputerDevice {
     public abstract int getPrice();
     public abstract int getPower();
