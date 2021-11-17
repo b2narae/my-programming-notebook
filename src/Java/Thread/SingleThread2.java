23dd67ed13a076a764f9c6acfd908b21d663c88b
TAEKHWAN <b2narae@gmail.com>
2021-11-17 23:53:21 +0900

    Code - Add SingleThread2 Example

---
+++ src/Java/Thread/SingleThread2.java
@@ -0,0 +1,36 @@
+package Java.Thread;
+
+// refer to : https://coding-factory.tistory.com/279
+
+public class SingleThread2 implements Runnable {
+
+    private int[] temp;
+
+    public SingleThread2() {
+        temp = new int[10];
+
+        for (int start = 0; start < temp.length; start++) {
+            temp[start] = start;
+        }
+    }
+
+    @Override
+    public void run() {
+        for (int start : temp) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+            System.out.println("Name of Thread : " + Thread.currentThread().getName());
+            System.out.println("temp value : " + start);
+        }
+    }
+
+    public static void main (String[] args) {
+        SingleThread2 st = new SingleThread2();
+        Thread t = new Thread(st, "first");
+
+        t.start();
+    }
+}
