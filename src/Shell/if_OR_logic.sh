ebd3f8a51115fcc3e3a8470a665b866737b52575
b2narae <b2narae@gmail.com>
2021-11-22 21:32:57 +0900

    Add : if statement with or logic

---
+++ src/Shell/if_OR_logic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter any number"
+read n
+
+if [[ ( $n -eq 15 || $n -eq 45 ) ]]
+then
+echo "You won the game"
+else
+echo "You lost the game"
+fi
+
+echo "Enter your number"
+read n
