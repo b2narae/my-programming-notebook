bd82da36d7abfa3ce4b3fe445766db989e7702ca
b2narae <b2narae@gmail.com>
2021-12-05 15:19:00 +0900

    chores : Editting Reflection Comment

--- src/DesignPattern/ObserverPattern/Messenger.java
+++ src/DesignPattern/ObserverPattern/Messenger.java
@@ -3,8 +3,9 @@ package DesignPattern.ObserverPattern;
 public class Messenger {
     public static void main(String[] args) {
         Company company = new Company();
-        Subscriber subscriver = new Subscriber(company);
+        Subscriber subscriber = new Subscriber(company);
 
         company.setMessage("photo", "new event");
+        System.out.println(company.hashCode());
     }
 }
